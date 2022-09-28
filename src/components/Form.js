db86ebe64fdab6d072663bd5794b2f4d6a9dc276
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-09-28 19:57:03 +0300

    commit tree 1d6a7f3eaa3d998c2ce1445714d133fc7490a6c5

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 
-function Form(props) {
+function Form() {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const[submitedInfo,setSubmitedInfo]=useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,12 +13,43 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
-  return (
-    <form>
+  function submitedData(event){
+event.preventDefault();
+
+const dataSubmited={firstName:firstName,lastName:lastName}
+
+const newSubmittedInfo=[...submitedInfo,dataSubmited];
+
+setSubmitedInfo(newSubmittedInfo);
+setFirstName("");
+setLastName("");
+
+console.log(submitedInfo,`Submitted info`);
+  }
+
+
+
+
+const dataToDisplay=submitedInfo.map((info,index)=>{
+
+return (<div key={index} >
+  <h1>FirstName is :{info.firstName}</h1>
+<h1>LastName is : {info.lastName}</h1>
+</div>
+)}
+
+)
+
+  return (<React.Fragment>
+     <form onSubmit={submitedData}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
+
+   {dataToDisplay}
+  </React.Fragment>
+
   );
 }
 
